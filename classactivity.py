44ef039966f6643452ba13fccf272da74968960e
Karma Chophey <02230090.cst@rub.edu.bt>
2024-03-03 18:33:38 +0530

    cw

---
+++ classactivity.py
@@ -0,0 +1,15 @@
+name = input("enter your name: ")
+# prompt the user to enter two values
+num1 = float(input("enter your first digit: "))
+num2 = float(input("enter your second digit: "))
+addition_result = num1 + num2
+print(f"additional_result = {addition_result}")
+subtraction_result = num1 - num2
+print(f"subtraction_result = {subtraction_result}")
+multiplication_result = num1 * num2
+print(f"multiplication_result = {multiplication_result}")
+division_result = num1 / num2
+print(f"division_resunt = {division_result}")
+print("------------------------------")
+print("its easy taugh")
+print("________________________________")
