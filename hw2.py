07b8d635218f302e074f9ddeb1077fdef0f48ef7
Karma Chophey <02230090.cst@rub.edu.bt>
2024-05-27 17:36:24 +0530

    hw2

---
+++ hw2.py
@@ -0,0 +1,26 @@
+# Initialize empty lists and dictionary to store student information
+students_list = []
+students_dict = {}
+# Add student information
+name = input("Enter student's name: ")
+age = int(input("Enter student's age: "))
+grade = int(input("Enter student's grade: "))
+students_list.append(name)
+students_dict [name] = {"age": age, "grade": grade}
+print("Student information added successfully!")
+print(students_dict.items())
+# Search for a student by name
+search_name = input("Enter the name of the student to search or simply enter to skip: ")
+if search_name in students_list:
+    info = students_dict [search_name]
+    print(f"Name: {search_name}, Age: {info['age']}, Grade: {info['grade']}")
+else:
+    print("Student not found!")
+# Remove a student
+remove_name = input("Enter the name of the student to remove or simply enter to skip: ")
+if remove_name in students_list:
+    del students_dict [remove_name]
+    students_list.remove(remove_name)
+    print("Student removed successfully!")
+else:
+    print("Student not found!")
